c56af5fa0a69ab6f5cd21189fdb38fd12ba322ce
Ntami Egbe <ntamiegbe00@gmail.com>
2022-08-12 22:56:33 +0100

    Initial Commit-Organizing file structure(12/08/22)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./style";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
